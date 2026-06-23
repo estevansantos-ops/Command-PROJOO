61fe91b8c0074a1da0162445ef2adb5dbe21b30d
Estevan <estevan.santos@unifesp.br>
2026-06-23 16:29:39 -0300

    feature: Implement Pessoa class with id and nome attributes

---
+++ Pessoa.java
@@ -0,0 +1,22 @@
+public class Pessoa {
+    private int id;
+    private String nome;
+
+    public Pessoa(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{id=" + id + ", nome='" + nome + "'}";
+    }
+}
